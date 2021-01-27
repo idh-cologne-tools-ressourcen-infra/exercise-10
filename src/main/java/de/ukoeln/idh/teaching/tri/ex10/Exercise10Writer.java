0c2b435601f1681d84f7a55819a3d91dcaa0ba4c
Corinna Munsch <cmunsch@smail.uni-koeln.de>
2021-01-27 20:40:13 +0100

    exercise 10
    the cloned main didn't work for me : the console gave out the following:
    SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder".
    SLF4J: Defaulting to no-operation (NOP) logger implementation
    SLF4J: See http://www.slf4j.org/codes.html#StaticLoggerBinder for further details.
    SLF4J: Failed to load class "org.slf4j.impl.StaticMDCBinder". ....

--- src/main/java/de/ukoeln/idh/teaching/tri/ex10/Exercise10Writer.java
+++ src/main/java/de/ukoeln/idh/teaching/tri/ex10/Exercise10Writer.java
@@ -1,14 +1,21 @@
 package de.ukoeln.idh.teaching.tri.ex10;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.SelectFSs;
 import org.apache.uima.jcas.JCas;
 import org.dkpro.core.api.io.JCasFileWriter_ImplBase;
+
+import de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity;
 
 public class Exercise10Writer extends JCasFileWriter_ImplBase {
 
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
-
+		
+//		von Aufgabe 9 
+//		SelectFSs<NamedEntity> selector = jcas.select(NamedEntity.class);
+//
+//		for (NamedEntity PER : selector) {}
 	}
 
 }

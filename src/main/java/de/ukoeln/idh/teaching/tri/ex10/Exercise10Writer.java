97aae570eaa28f3b12c26680a4274d909a82e7a8
Valeria Gavrilova <gavrivalery@gmail.com>
2021-01-28 11:05:03 +0100

    boundary matcher added. TODO: writer :sparkles:

--- src/main/java/de/ukoeln/idh/teaching/tri/ex10/Exercise10Writer.java
+++ src/main/java/de/ukoeln/idh/teaching/tri/ex10/Exercise10Writer.java
@@ -1,29 +1,73 @@
 package de.ukoeln.idh.teaching.tri.ex10;
 
 import de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity;
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.tcas.Annotation;
 import org.dkpro.core.api.io.JCasFileWriter_ImplBase;
 
-import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Exercise10Writer extends JCasFileWriter_ImplBase {
-	List<Annotation> items = new LinkedList<Annotation>();
 
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
-		for(Sentence sentence : jcas.select(Sentence.class)){
-			for (Annotation a : jcas.select(Annotation.class).coveredBy(sentence)){
-				items.add(a);
-			}
-		}
-		
+			for(NamedEntity ne : jcas.select(NamedEntity.class)){
 
+				List<Token> leftTokenList = jcas.select(Token.class).preceding(ne).asList();
+				int leftTokensSize = leftTokenList.size();
+				List<Token> rightTokenList = jcas.select(Token.class).following(ne).asList();
+				int rightTokensSize = rightTokenList.size();
+
+				if(leftTokensSize >= 2){
+					String item = leftTokenList.get(leftTokensSize-2).getCoveredText();
+
+					if(!punctuationMatcher(item)){
+						System.out.print(item + " ");
+					}
+
+				}
+				if(leftTokensSize >= 1){
+					String item = leftTokenList.get(leftTokensSize-2).getCoveredText();
+					if(!punctuationMatcher(item)){
+						System.out.print(item + " ");
+					}
+
+				}
+				System.out.print(ne.getCoveredText() + " ");
+
+				if(rightTokensSize >= 1){
+					String item = rightTokenList.get(0).getCoveredText();
+					if(!punctuationMatcher(item)){
+						System.out.print(item + " ");
+					}
+
+				}
+				if(rightTokensSize >= 2){
+					String item = rightTokenList.get(1).getCoveredText();
+					if(!punctuationMatcher(item)){
+						System.out.print(item + "\n");
+					}
+
+				}
+
+
+			}
+
+	}
+
+	/**
+	 * Matches punctuation tokens
+	 * @param item
+	 * @return a boolean if punctuation mark was found
+	 */
+	private boolean punctuationMatcher(String item){
+		Pattern p = Pattern.compile("[.,!?()/\\-]");
+		Matcher m = p.matcher(item);
+		return m.find();
 	}
 
 }
